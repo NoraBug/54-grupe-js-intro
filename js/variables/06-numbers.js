bece9592a6ffbbdd87aec7b2bf4ac4effa8fd58a
NoraBug <eleonora0303@gmail.com>
2025-02-04 14:38:12 +0200

    miau4

--- js/variables/06-numbers.js
+++ js/variables/06-numbers.js
@@ -25,11 +25,8 @@ console.log(n5);
 const n6=-Infinity;
 console.log(n6);
 
-const n7=-99999999;
+const n7=NaN;
 console.log(n7);
-
-const n8=NaN;
-console.log(n8);
 
 console.log('labas rytas'/2);
 
@@ -40,10 +37,41 @@ console.log(2**0);
 console.log(2**1);
 console.log(2**2);
 
+console.clear();
+
 // Math- matematiniu funkciju "biblioteka"
+// js math
 
+console.log('matematines konstantos');
+console.log(Math.E);
+console.log(Math.PI);
 
+console.log('trigonometrija - naudoja radianus')
+console.log(Math.sin(2));
 
+console.log('kubine saknis');
+console.log(Math.cbrt(27));
+
+console.log('apvalinimas: ceil');
+console.log(Math.ceil(10));
+console.log(Math.ceil(9.6));
+console.log(Math.ceil(9.2));
+console.log(Math.ceil(8.5));
+
+console.log('apvalinimas: floor');
+console.log(Math.floor(10));
+console.log(Math.floor(9.8));
+console.log(Math.floor(8.7));
+
+console.log('apvalinimas: round');
+console.log(Math.round(7.8));
+console.log(Math.round(7.4));
+
+console.log('"apvalinimas"; trunc');
+console.log(Math.trunc(10));
+console.log(Math.trunc(9.5));
+console.log(Math.trunc(-10));
+console.log(Math.trunc(-9.99));
 
 
 
